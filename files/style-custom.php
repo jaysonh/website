b134dcbe79aa3b56bf11928ec6754300e7b13083
jayson <mail@jaysonh.com>
2013-11-01 16:50:09 +0000

    changes

---
+++ files/style-custom.php
@@ -0,0 +1,106 @@
+/*** Google Font ***/
+
+
+/*** Basic Font ***/
+body{
+	font-family:Helvetica, "Helvetica Neue", Arial, Sans-Serif;
+}
+
+
+/*** Background Color ***/
+body{
+		background-color: #ffffff;
+	}
+
+/*** Background Image ***/
+body {
+		
+}
+
+/*** Text Color ***/
+body{
+		color:#555555;
+	}
+
+#nav li a,
+.widget a,
+a.scroll-top,
+.entry-title a{
+		color:#555555;
+	}
+#nav ul > li.current-menu-item > a,
+#nav a:hover,
+.widget a:hover,
+.entry-title a:hover{
+		color:#0088CC;
+	}
+
+#nav ul > li.current-menu-item > a,
+#nav a:hover{
+		background-color:#fafafa;
+	}
+
+/*** Nav Lines ***/
+.portfolio-thumbs .thumb a:hover,
+#header, #content, #nav, #nav a {
+	border-color:#ededed;
+}
+.segment-sep{
+	background-color:#ededed;
+}
+
+/*** Link Color ***/
+a{
+	color: #0088CC;
+}
+
+/*** Title Color ***/
+h1, h2, h3, h4, h5, h6, .entry-title, .widget-title{
+	color: #dfdfdf;
+}
+
+
+/*** Footer Color ***/
+#footer,
+#footer .copyright{
+	color: #888888;
+}
+
+
+.entry-meta a {
+	color:#;
+}
+.entry-meta a:hover{
+	color:#;
+}
+
+.entry-meta a:hover{
+	color:#;
+}
+
+/*** Compensate Admin Bar ***/
+
+/*** Menu Font Size and Line Height ***/
+#nav{
+	font-size: 12px;
+}
+
+#nav li{
+	line-height: 1.5;
+}
+
+
+/*** Page Headings Font Size ***/
+.entry-title{
+	font-size: 24px;
+}
+
+/*** Page Headings Font Size ***/
+.entry-title{
+	font-size: 24px;
+}
+
+/*** Paragraph Font Size ***/
+.entry-content{
+	font-size: 12px;
+}
